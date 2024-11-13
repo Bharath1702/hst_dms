36a962b45268fe2fa62d82d02434ceb0a02a70ed
Bharath1702 <bharathac7@gmail.com>
2024-11-13 17:21:31 +0530

    v-1

--- client/src/UserDetails.js
+++ client/src/UserDetails.js
@@ -113,7 +113,7 @@ END:VCARD
             <Button variant="primary" onClick={handleSaveContact}>
               Save Contact
             </Button>
-            <center style={'color:white'}>
+            <center color='white'>
               powered by <a href="https://blueticksinnovations.com">Blueticks Innovations Private Limited</a>
             </center>
           </Card.Body>
